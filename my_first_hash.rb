044eb51c59c5699f06f27f1fdb681fb3136d6f5f
Katie Cassell <kcassell944@gmail.com>
2018-02-23 18:29:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,9 +37,9 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+return
 
   # return the shipping_manifest hash below
-shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
+shipping_manifest["muskets"]
+shipping_manifest["gun powder"] 
 end
